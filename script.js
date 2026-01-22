c2f8a4877c50e4d3b0df6c3c42434ff836434ed1
Miggi97G <m.famgarcia@outlook.com>
2026-01-22 16:33:09 +0100

    Initial

---
+++ script.js
@@ -0,0 +1,8 @@
+//Notizen global speichern
+//Notizen laden
+//Notizen Hinzufügen
+//Notizen löschen
+//Notizen verschiebn (Drag and Drop)
+//Notizen bearbeiten
+//Notizen suchen
+//Notiz Datum (erstellt am)
