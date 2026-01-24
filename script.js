6f5c338a42451a3132d670e15d5cf8e4b54ed42c
Miggi97G <m.famgarcia@outlook.com>
2026-01-24 16:09:22 +0100

    Update

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
 //Notizen global speichern
-//Notizen laden
 //Notizen Hinzufügen
 //Notizen löschen
+
+//Erledigt Button
+//Erledigt Bereich
 //Notizen verschiebn (Drag and Drop)
 //Notizen bearbeiten
 //Notizen suchen
 //Notiz Datum (erstellt am)
+//Papierkorb, wird nach 30 tagen gelöscht
+
+let notes = [];
+
+function renderNotes() {
+    const contentDiv = document.getElementById('content');
+    for (let indexNote = 0; indexNote < notes.length; indexNote++) {
+        
+        contentDiv.innerHTML += getNoteTemplate(indexNote);
+    }
+}
+
+function getNoteTemplate(indexNote) {
+    return `<div class="note">
+       <button onclick="deleteNote(${indexNote})">X</button> ${notes}
+    </div>`;
+}
+
+function addNote() {
+   let noteInputRef = document.getElementById('note_input');
+   let noteInput = noteInputRef.value
+
+    notes.push(noteInput);
+    renderNotes();
+}
+
+function deleteNote(indexNote) {
+    notes.splice(indexNote, 1);
+    renderNotes();
+}
